ff91a06165b23899c0110caf7bb764d7fa749dda
Francisco J. Medina <fjmedina21@gmail.com>
2023-03-15 00:14:12 -0400

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -14,7 +14,7 @@ export const config = {
 	JWT_SESSION_EXPIRES_IN: "4h",
 	JWT_RESET_TOKEN_SECRECT: "pr1V@t3K3yr3sEtT0k3n",
 	JWT_RESET_TOKEN_EXPIRES_IN: "10m",
-	JWT_COOKIE_EXPIRES_IN_DAY: 10 * 86400000 // --> 24h*60min*60s*1000ms => 86,400,000ms => 1d
+	JWT_COOKIE_EXPIRES_IN_DAY: 1 * 86400000 // --> 24h*60min*60s*1000ms => 86,400,000ms => 1d
 };
 
 export {};
