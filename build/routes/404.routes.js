599faae328cc25efdfa4f2497561d712d092105a
Francisco J. Medina <fjmedina21@gmail.com>
2023-04-17 23:39:14 -0400

    Delete build/routes directory

--- build/routes/404.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.$404Route = void 0;
-const express_1 = require("express");
-const $404Route = (0, express_1.Router)();
-exports.$404Route = $404Route;
-$404Route.all("*", (req, res) => {
-    res.status(404).send("<h1>404! Page Not Found</h1>");
-});
--- build/routes/home.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HomeRoute = void 0;
-const express_1 = require("express");
-const HomeRoute = (0, express_1.Router)();
-exports.HomeRoute = HomeRoute;
-HomeRoute.get("/", (req, res) => {
-    res.status(200).send("<h1>Home</h1>");
-});
